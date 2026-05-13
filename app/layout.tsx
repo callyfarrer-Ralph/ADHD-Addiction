b580b78a5b6361f0c0a9ebda301f023ee4e8a73c
callyfarrer-Ralph <cally.farrer@outlook.com>
2026-05-13 22:47:23 +0100

    Add root layout

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-white text-slate-900">{children}</body>
+    </html>
+  )
+}
